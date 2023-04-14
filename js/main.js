e20d9644f50323481eedf51a343dd017ab278399
Muhamed bassal <69437958+Bassal-11@users.noreply.github.com>
2023-04-14 15:48:13 +0200

    uploading files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* global alert */
+export function testme() {
+    alert('hi!');
+}
+
+$( document ).ready(function($) {
+	$('.js-tilt').tilt({
+	})   
+
+});
+
+
---
+++ nunjuckspages.js
@@ -0,0 +1,49 @@
+module.exports = [
+  {
+    from: 'templates/index.njk',
+    to: 'index.html'
+  },
+  {
+    from: 'templates/anotherpage.njk',
+    to: 'anotherpage.html'
+  },
+  {
+    from: 'templates/company.njk',
+    to: 'company.html'
+  },
+  {
+    from: 'templates/services.njk',
+    to: 'services.html'
+  },
+  {
+    from: 'templates/works.njk',
+    to: 'works.html'
+  },
+  {
+    from: 'templates/blog.njk',
+    to: 'blog.html'
+  }, 
+  {
+    from: 'templates/single-post.njk',
+    to: 'single-post.html'
+  },
+  {
+    from: 'templates/contact.njk',
+    to: 'contact.html'
+  },
+  {
+    from: 'templates/loader.njk',
+    to: 'loader.html'
+  },
+  {
+    from: 'templates/components.njk',
+    to: 'components.html'
+  },
+  {
+    from: 'templates/test.njk',
+    to: 'test.html'
+  }
+];
+
+
+              
